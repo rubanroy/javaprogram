1d824719331f3dbae11b0f5bb64cab943312a716
RUBAN <105547591+rubanroy@users.noreply.github.com>
2024-09-07 11:14:28 +0530

    Create inheritence.java

---
+++ inheritence.java
@@ -0,0 +1,41 @@
+class Calculater
+{
+	public int add(int n1, int n2)
+	{
+		return n1+n2;
+	}
+	public int sub(int n1, int n2)
+	{
+		return n1-n2;
+	}
+}
+
+
+public class AdvanceCalc extends Calculater
+{
+	public int multi(int n1, int n2)
+	{
+		return n1*n2;
+	}
+	public int div(int n1, int n2)
+	{
+		return n1/n2;
+	}
+}
+
+
+public class Demo
+{
+	public static void main(String a[]) 
+	{
+//		Calculater obj=new Calculater();
+		AdvanceCalc obj=new AdvanceCalc();
+		int r1=obj.add(4, 5);
+		int r2=obj.sub(7,3);
+		int r3=obj.multi(5,3);
+		int r4=obj.div(15,4);
+		
+		System.out.println(r1+" "+r2);
+		
+	}
+}
